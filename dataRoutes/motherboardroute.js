e598caca7beee8844598540bb26b23dde085eb54
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-07-24 11:39:13 +0800

    Changed variables to camelCase

--- dataRoutes/motherboardroute.js
+++ dataRoutes/motherboardroute.js
@@ -1,33 +1,33 @@
 const express = require("express");
-const motherboardrouter = express.Router();
+const motherboardRouter = express.Router();
 const motherboardModel = require("./../models/motherboard");
 
 //GET all the motherboard data
-motherboardrouter.get("/", async (req, res, next) => {
+motherboardRouter.get("/", async (req, res, next) => {
   const motherboards = await motherboardModel.find();
-  
+
   if (req.query == {}) {
     res.status(200).json(motherboards);
   } else {
-    const requestedModel = req.query.model.toLowerCase()
+    const requestedModel = req.query.model.toLowerCase();
 
     const filteredBoards = motherboards.filter(board => {
-      currentBoardName = board.model.toLowerCase()
-      return currentBoardName.includes(requestedModel)
-    })
+      currentBoardName = board.model.toLowerCase();
+      return currentBoardName.includes(requestedModel);
+    });
 
-    res.status(200).json(filteredBoards)
+    res.status(200).json(filteredBoards);
   }
 });
 
 //GET only a specific motherboard model data
-motherboardrouter.get("/:id", async (req, res, next) => {
+motherboardRouter.get("/:id", async (req, res, next) => {
   const findMotherboard = await motherboardModel.findById(req.params.id);
   res.json(findMotherboard);
 });
 
 //POST motherboard data
-motherboardrouter.post("/", async (req, res, next) => {
+motherboardRouter.post("/", async (req, res, next) => {
   const newMotherboard = new motherboardModel({
     id: req.body.id,
     model: req.body.model,
@@ -39,7 +39,7 @@ motherboardrouter.post("/", async (req, res, next) => {
 });
 
 //PUT motherboard data via id number
-motherboardrouter.put("/:id", async (req, res, next) => {
+motherboardRouter.put("/:id", async (req, res, next) => {
   const testing = await motherboardModel.findByIdAndUpdate(
     req.params.id,
     req.body
@@ -47,12 +47,12 @@ motherboardrouter.put("/:id", async (req, res, next) => {
   res.status(204).json();
 });
 //DELETE motherboard by its id number
-motherboardrouter.delete("/:id", async (req, res, next) => {
+motherboardRouter.delete("/:id", async (req, res, next) => {
   await motherboardModel.findByIdAndDelete(req.params.id, req.body);
   res.status(204).json();
 });
 //Export router in a mongodb use case
 module.exports = app => {
   app.use(express.json());
-  app.use("/motherboard", motherboardrouter);
+  app.use("/motherboards", motherboardRouter);
 };
